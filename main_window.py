c6caa4f48a35e125579b9c3480f114dd720fa332
vhangout <none@email>
2021-10-21 17:25:59 +0500

    up

--- main_window.py
+++ main_window.py
@@ -2,7 +2,10 @@ from PIL import Image
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QMainWindow, QComboBox, QFileDialog, QMessageBox
 from PyQt5.uic import loadUi
+
+from grid_utils import GridUtils
 from pagesizes import pagesizes
+from reportlab.lib.units import mm
 from skin_utils import SkinUtils
 
 
@@ -20,6 +23,8 @@ class MainWindow(QMainWindow):
         self.cbGridPaperSize.addItems(pagesizes.keys())
         self.setComboboxValue(self.cbGridPaperSize, 'A4')
 
+        self.pbGridSavePDF.clicked.connect(self.saveGridPDF)
+
         self.errorDialog = QMessageBox()
         self.errorDialog.setIcon(QMessageBox.Critical)
         self.errorDialog.setText("Error")
@@ -27,9 +32,9 @@ class MainWindow(QMainWindow):
 
     @pyqtSlot()
     def browsePNG(self):
-        fname = QFileDialog.getOpenFileName(self, 'Open Maincraft skin PNG file', filter='Images (*.png)')
-        if fname[0]:
-            self.leSkinPNGFile.setText(fname[0])
+        fname = QFileDialog.getOpenFileName(self, 'Open Maincraft skin PNG file', filter='Images (*.png)')[0]
+        if fname:
+            self.leSkinPNGFile.setText(fname)
 
     @pyqtSlot()
     def saveSkinPDF(self):
@@ -38,15 +43,42 @@ class MainWindow(QMainWindow):
             self.errorDialog.exec_()
             return
 
-        fname = QFileDialog.getSaveFileName(self, 'Save PDF Document', filter='PDF Documents (*.pdf)')
-        if fname[0]:
+        fname = QFileDialog.getSaveFileName(self, 'Save PDF Document', filter='PDF Documents (*.pdf)')[0]
+        if fname:
             skin = Image.open(self.leSkinPNGFile.text())
             data = list(skin.getdata())
             pixelmap = [data[n * 64:(n + 1) * 64] for n in range(64)]
 
-            draw_utils = SkinUtils(pixelmap
-                                   )#, pdf_file_name, mob_name)  # , place_mask_on_head=False)
+            draw_utils = SkinUtils(pixelmap=pixelmap,
+                                   pdf_file_name=fname,
+                                   mob_name=self.leMobName.text(),
+                                   pdf_pagesize=str(self.cbSkinPaperSize.currentText()),
+                                   pdf_landscape='Landscape' == str(self.cbSkinPageOrientation.currentText()),
+                                   pdf_left_bound=self.sbSkinMarginHor.value() * mm,
+                                   pdf_top_bound=self.sbSkinMarginVert.value() * mm,
+                                   pdf_grid_size=self.sbSkinGridSize.value() * mm,
+                                   pdf_face_padding=self.sbSkinFacePadding.value() * mm,
+                                   pdf_part_padding=self.sbSkinPartPadding.value() * mm,
+                                   face_flip_horizontally=self.checkSkinFlipHor.isChecked(),
+                                   place_mask_on_head=self.checkSkinMergeMask.isChecked())
             draw_utils.draw_skin()
+
+    @pyqtSlot()
+    def saveGridPDF(self):
+        if not self.leGridGridsSizes.text():
+            self.errorDialog.setInformativeText('Please select grid sizes')
+            self.errorDialog.exec_()
+            return
+
+        fname = QFileDialog.getSaveFileName(self, 'Save PDF Document', filter='PDF Documents (*.pdf)')[0]
+        if fname:
+            grid_utils = GridUtils(pdf_file_name=fname,
+                                   pdf_pagesize='A4',
+                                   pdf_landscape='Landscape' == str(self.cbGridPageOrientation.currentText()),
+                                   pdf_left_bound=self.sbGridMarginHor.value() * mm,
+                                   pdf_top_bound=self.sbGridMarginVert.value() * mm,
+                                   pdf_padding=3 * mm)
+            grid_utils.draw_grids()
 
     @staticmethod
     def setComboboxValue(cb: QComboBox, value: str):
